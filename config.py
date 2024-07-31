b28d49246c5aa6ae7c974e5423c3f568c7495b86
Max Platonov <bliamba2000@gmail.com>
2024-07-31 15:12:52 +0400

    Попробовал исправить момент со слабой структурой

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
 import json
+from json import JSONDecodeError
 from typing import Any
 
 
@@ -6,8 +7,12 @@ class ConfigLoader:
     """Класс для загрузки конфигурационных данных, таких как сообщения и статусы."""
 
     def __init__(self, config_path: str):
-        with open(config_path, 'r', encoding='utf-8') as file:
-            self.config = json.load(file)
+        try:
+            with open(config_path, 'r', encoding='utf-8') as file:
+                self.config = json.load(file)
+        except (FileNotFoundError, JSONDecodeError) as e:
+            print(f"Error loading config: {e}")
+            self.config = {}
 
     def get(self, key: str) -> Any:
         return self.config.get(key)
